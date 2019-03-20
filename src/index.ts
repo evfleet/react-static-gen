06cd3d8a1a42a50449fa59c31f1e99908d7d7404
Evan Fleet <evfleet@gmail.com>
2019-03-19 21:59:32 -0400

    build: setup eslint
    setup eslint for the project with typescript, prettier and airbnb

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,31 @@
-import fs from 'fs';
-import path from 'path';
-import util from 'util';
-import webpack from 'webpack';
-import HTMLPlugin from 'html-webpack-plugin';
+import fs from "fs";
+import path from "path";
+import util from "util";
+import webpack from "webpack";
+import HTMLPlugin from "html-webpack-plugin";
 
 const readFiles = util.promisify(fs.readdir);
 
 export default async () => {
-  const templates = await readFiles('templates');
+  const templates = await readFiles("templates");
 
-  console.log('templates', templates)
-  
-  const compiler = webpack({ 
-    entry: './scripts/index.js',
+  console.log("templates", templates);
+
+  const compiler = webpack({
+    entry: "./scripts/index.js",
     output: {
-      path: path.join(process.cwd(), 'public')
+      path: path.join(process.cwd(), "public")
     },
     plugins: [
       new HTMLPlugin({
-        filename: 'test.html',
-        template: 'templates/index.html'
+        filename: "test.html",
+        template: "templates/index.html"
       })
-    ] 
+    ]
   });
 
   compiler.run((err, stats) => {
-    console.log('err', err);
-    console.log('stats', stats);
-  })
-}
+    console.log("err", err);
+    console.log("stats", stats);
+  });
+};
